e2ae41a474d42b6e78f88644aab622d6648619f6
alparo <amparo.dios@gmail.com>
2022-08-05 13:57:13 +0300

    Almost ready.

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
-from sendoutnft import find_asset_with_highest_mint, send_assets_to_wallet, SENDER, prepare_list_of_templates
-from loguru import logger
+from sendoutnft import  prepare_list_of_templates, \
+    send_nfts_to_user, COLLECTION_WALLET
 
 INPUT = [
     "Grass Tuft",
     "Dried Leaf",
     "Dried Leaf",
-    "Pinecone",
-    "Big Flat Stone",
-    "Granite Stone",
-    "Grass Tuft",
-    "Dried Leaf",
-]
+    "Pinecone",]
+#     "Big Flat Stone",
+#     "Granite Stone",
+#     "Grass Tuft",
+#     "Dried Leaf",
+# ]
 
+recipient = "thisismyseco"
 
-print(prepare_list_of_templates(INPUT))
-
-template_id1 = "54"
-assets_to_send = None
-#assets_to_send = find_asset_with_highest_mint(SENDER, template_id1, 2)
-if assets_to_send:
-    tx_return_status = send_assets_to_wallet(assets_to_send, "thisismyseco")
-    if tx_return_status:
-        logger.info(f"Successful: {tx_return_status}")
-    else:
-        logger.error(f"TX failed")
+send_nfts_to_user(recipient, COLLECTION_WALLET, INPUT)
--- sendoutnft.py
+++ sendoutnft.py
@@ -4,15 +4,15 @@ from loguru import logger
 
 import requests
 
+from collections import Counter
 
 ENTRYPOINT = "https://test.wax.api.atomicassets.io/atomicassets/v1"
 ENTRYPOINT_ASSETS = f"{ENTRYPOINT}/assets"
-#KEY = os.environ["PRIVATE_KEY"]
+# KEY = os.environ["PRIVATE_KEY"]
 # SENDER = "pixeltycoons"
-SENDER = "thisismyfirs"
+COLLECTION_WALLET = "thisismyfirs"
 COLLECTION = "gen1pokemon1"
 logger.add("debug.log")
-
 
 DICT = {
     "Grass Tuft": 289228,
@@ -37,7 +37,7 @@ def send_assets_to_wallet(assets: list, receiver: str, memo: str = ""):
     data = [
         eospyo.Data(
             name="from",
-            value=eospyo.types.Name(SENDER),
+            value=eospyo.types.Name(COLLECTION_WALLET),
         ),
         eospyo.Data(
             name="to",
@@ -53,7 +53,7 @@ def send_assets_to_wallet(assets: list, receiver: str, memo: str = ""):
         ),
     ]
 
-    auth = eospyo.Authorization(actor=SENDER, permission="active")
+    auth = eospyo.Authorization(actor=COLLECTION_WALLET, permission="active")
 
     action = eospyo.Action(
         account="atomicassets",  # this is the contract account
@@ -82,14 +82,14 @@ def send_assets_to_wallet(assets: list, receiver: str, memo: str = ""):
         return False
 
 
-def get_assets(wallet, collection, template, limit, sorting_key="template_mint"):
+def get_available_assets(collection_wallet, collection, template, limit, sorting_key="template_mint"):
     """
     Make request to blockchain to get available assets with given template_id.
-    wallet - is the collection wallet with alle the assets.
+    collection_wallet - is the collection wallet with all the assets.
     Result is by default sorted by highest template mint.
     """
     payload = {
-        "owner": wallet,
+        "owner": collection_wallet,
         "collection_name": collection,
         "template_id": template,
         "limit": limit,
@@ -99,18 +99,23 @@ def get_assets(wallet, collection, template, limit, sorting_key="template_mint")
     return response.json()
 
 
-def find_asset_with_highest_mint(wallet, template_id, quantity_requested: int = 1):
+def find_asset_with_highest_mint(collection_wallet, template_id, quantity_requested: int = 1):
+    """
+    Finds in collection_wallet given quantity of assets with given template.
+    :param collection_wallet:
+    :param template_id:
+    :param quantity_requested:
+    :return: list of found asset IDs
+    """
     # make a request to blockchain to get available assets with given template_id
-    assets = get_assets(wallet, COLLECTION, template_id, quantity_requested)
+    assets = get_available_assets(collection_wallet, COLLECTION, template_id, quantity_requested)
     # check if enough assets are available in collection wallet
     quantity_available = len(assets["data"])
     if quantity_available < quantity_requested:
         logger.error(
-            f"Not enough assets available.\n"
-            f"you have: {quantity_available}\n"
-            f"you need: {quantity_requested}"
+            f"Not enough assets available. Have: {quantity_available}; Need: {quantity_requested}"
         )
-        # TO-DO here will come the logic to mint more assets.
+        # TO-DO here will come the logic to mint missing assets.
         return
     # extract asset_ids from reply data
     asset_ids = [data["asset_id"] for data in assets["data"]]
@@ -118,5 +123,45 @@ def find_asset_with_highest_mint(wallet, template_id, quantity_requested: int = 
 
 
 def prepare_list_of_templates(asset_names):
-    list_of_templates = [DICT[asset] for asset in asset_names]
-    return list_of_templates
+    """
+    :param asset_names: list with asset names e.g. ["Grass Tuft", "Dried Leaf", "Dried Leaf", "Pinecone"]
+    :return: class dict_items (list of tuples) with templates and their quantities.
+             E.g. [(289228, 2), (289221, 1), (529844, 1)]
+    """
+    # Translate asset names into template IDs
+    list_of_templates = []
+    for asset in asset_names:
+        try:
+            list_of_templates.append(DICT[asset])
+        except KeyError as err:
+            logger.error(f"Asset name '{err}' is not in the dictionary and will not be sent.")
+    # Count duplicates of template IDs and creates dictionary
+    dict_with_counted_templates = Counter(list_of_templates)
+    return dict_with_counted_templates.items()
+
+
+def send_nfts_to_user(wallet: str, collection_wallet: str, asset_names: list):
+    """
+    :param collection_wallet: collection wallet with all the assets
+    :param wallet: recipient wallet
+    :param asset_names: list with asset names e.g. ["Grass Tuft", "Dried Leaf", "Dried Leaf", "Pinecone"]
+    :return: TX ID or 'False' if TX failed.
+    """
+    templates = prepare_list_of_templates(asset_names)
+    assets_to_send = []
+    for template, quantity in templates:
+        logger.info(f"Trying to send {quantity} asset(s) of template {template}")
+        try:
+            assets_to_send.extend(find_asset_with_highest_mint(COLLECTION_WALLET, template, quantity))
+        except:
+            logger.error(f"Can't send asset with template '{template}'")
+
+    assets_to_send = None
+    if assets_to_send:
+        logger.error("not here")
+        tx_return_status = send_assets_to_wallet(assets_to_send, wallet)
+        if tx_return_status:
+            logger.info(f"Successful: {tx_return_status}")
+        else:
+            logger.error(f"TX failed")
+    pass
